eb5b533da287ff1e5593769278444aa669a4a5e8
Gurucharan Chouhan <78747692+guruchara@users.noreply.github.com>
2022-04-07 21:35:01 +0530

    Time: 6 ms (35.59%), Space: 43 MB (38.84%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,42 @@
+class Solution {
+    public static String [] coder = {"","","abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv","wxyz"};
+    ArrayList<String>p=new ArrayList<String>();
+    public List<String> letterCombinations(String s) {           
+        if(s.equals(""))
+        {
+            return p;
+        }
+        else
+        {
+            return check(s);
+        }
+    }
+        
+        public static List<String> check(String s)
+        {
+          if(s.length()==0)
+          {
+              ArrayList<String>l=new ArrayList<String>();          
+              l.add("");
+              return l;
+          }
+        char ch=s.charAt(0);
+        String x=s.substring(1);
+        
+        List<String> recAns =  check(x);
+        ArrayList<String> myAns = new ArrayList<>();
+        
+        int idx=ch-'0';
+        String k=coder[idx];
+        
+        for(int i=0;i<k.length();i++)
+        {
+            char newchar=k.charAt(i);
+            for(String m:recAns)
+            {
+                myAns.add(newchar+m);
+            }
+        }               
+         return myAns;        
+    }
+}
