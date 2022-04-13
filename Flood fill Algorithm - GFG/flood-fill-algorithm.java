a95f637c90dcbc04644eb36b09bcfddb78a759ce
Gurucharan Chouhan <78747692+guruchara@users.noreply.github.com>
2022-04-13 16:52:17 +0530

    Added solution - LeetHub

---
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.java
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] S1 = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(S1[0]);
+            int m = Integer.parseInt(S1[1]);
+            int[][] image =  new int[n][m];
+            for(int i = 0; i < n; i++){
+                String[] S2 = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++)
+                    image[i][j] = Integer.parseInt(S2[j]);
+            }
+            String[] S3 = br.readLine().trim().split(" ");
+            int sr = Integer.parseInt(S3[0]);
+            int sc = Integer.parseInt(S3[1]);
+            int newColor = Integer.parseInt(S3[2]);
+            Solution obj = new Solution();
+            int[][] ans = obj.floodFill(image, sr, sc, newColor);
+            for(int i = 0; i < ans.length; i++){
+                for(int j = 0; j < ans[i].length; j++)
+                    System.out.print(ans[i][j] + " ");
+                System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    public static int[][] dirs={
+        {0,1},
+        {1,0},
+        {0,-1},
+        {-1,0}
+    };
+    public int[][] floodFill(int[][] a, int sr, int sc, int newColor)
+    {
+     solve(a,sr,sc,newColor);
+        return a;
+    }
+    private static void solve(int[][] a,int r,int c,int ncol){
+        int cc=a[r][c];
+        a[r][c]=-1;
+        for(int[] d:dirs){
+            int nr=r+d[0];
+            int nc=c+d[1];
+            if(nr>=0 && nc>=0 && nr<a.length && nc<a[0].length && a[nr][nc]==cc){
+                solve(a,nr,nc,ncol);
+            }
+        }
+        a[r][c]=ncol;
+    }
+}
