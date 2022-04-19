cbe7d4319b7b684315ed62a12598cf272f158e82
Gurucharan Chouhan <78747692+guruchara@users.noreply.github.com>
2022-04-19 18:13:31 +0530

    Time: 664 ms (14.88%), Space: 46.7 MB (81.49%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,95 +1,41 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] num) {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> threeSumSet  = new HashSet<>();
         
-//          if(nums.length==0 || nums.length==1)
-//          {
-//              List<List<Integer>>k=new ArrayList<>();
-//              return k;
-//          }
-//         HashSet<List<Integer>>lh=new HashSet<>();
+        Arrays.sort(nums);
         
-//         // LinkedHashSet<List<Integer>>h=new LinkedHashSet<>()
-//         HashMap<Integer,Integer>hm=new HashMap<>();
-        
-//         for(int i=0;i<nums.length;i++)
-//         {
-//             hm.put(nums[i],1);
-//         }
-        
-//         HashSet<Integer>h=new HashSet<Integer>();
-//           for(int val:nums)
-//           {
-//               h.add(val);
-//           }
-        
-        
-//         if(h.size()==1)
-//         {
-//              List<List<Integer>>k=new ArrayList<>();
+        // length-2  bcz hme kvl three elemenet ka sum check krna he 
+        for(int i=0; i<nums.length-2;i++){ 
+            int j =i+1;
+            
+            int k = nums.length-1;
+            
+            // kabhi hm cross kr denge to loop se bahar jayenege
+            while(j<k){
+                
+                int sum = nums[i]+nums[j]+nums[k];
 
-//             ArrayList<Integer>l=new ArrayList<>();
-//              for(int x:nums)
-//              {
-//                  l.add(x);
-//              }            
-//             k.add(l);
-//             return k;
-//         }
-                        
-//         for(int i=0;i<nums.length-1;i++)
-//         {
-//            for(int j=i+1;j<nums.length;j++)
-//            {
-//                int sum=nums[i]+nums[j];
-//                ArrayList<Integer>l=new ArrayList<>();                
-//                if(hm.containsKey(sum) && nums[i]!=sum && nums[j]!=sum)
-//                {
-//                     l.add(nums[i]);
-//                     l.add(nums[j]);
-//                     int x=(-1)*sum;
-//                     l.add(x);
-//                }
-               
-         
-//                if(l.size()==3)
-//                {                  
-//                    Collections.sort(l);
-//                    lh.add(l);
-//                }
-//            }
-//         }
-//         return new ArrayList<>(lh);
-        
-        
-        Arrays.sort(num);
-
-        List <List < Integer >> res = new ArrayList < > ();
-
-        for (int i = 0; i < num.length - 2; i++) {
-
-            if (i == 0 || (i > 0 && num[i] != num[i - 1])) {
-
-                int lo = i + 1, hi = num.length - 1, sum = 0 - num[i];
-
-                while (lo < hi) {
-                    if (num[lo] + num[hi] == sum) {
-                        ArrayList < Integer > temp = new ArrayList < > ();
-                        temp.add(num[i]);
-                        temp.add(num[lo]);
-                        temp.add(num[hi]);
-                        res.add(temp);
-
-                        while (lo < hi && num[lo] == num[lo + 1]) lo++;
-                        while (lo < hi && num[hi] == num[hi - 1]) hi--;
-
-                        lo++;
-                        hi--;
-                    } else if (num[lo] + num[hi] < sum) lo++;
-
-                    else hi--;
+            // if sum is equal to 0 then j increment or k ko decrement krenge and i to vese bhi increment ho jayega 
+                
+                if(sum==0){
+                    threeSumSet.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    
+                    j++;
+                    k--;
                 }
+           // sum jyada hota  tb k ko km krenege kuyki hme ek bar k vale pe sum==0 mil chuka
+                else if (sum > 0){
+                    k--;
+                } 
+                
+                // in this case only move the j 
+                else if (sum < 0){
+                    j++;
+                } 
             }
+
         }
-        return res;
+        
+        return new ArrayList<>(threeSumSet);
     }
 }
