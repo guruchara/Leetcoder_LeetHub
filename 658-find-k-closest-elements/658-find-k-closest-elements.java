52016b43185dabb1e49fec377b3b3d11b528997d
Gurucharan Chouhan <78747692+guruchara@users.noreply.github.com>
2022-05-26 17:05:52 +0530

    Time: 73 ms (6.70%), Space: 69.8 MB (5.08%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,101 +1,103 @@
-// class Solution {
+class Solution {
     
-//     class pair
-//     {
-//         int key;
-//         int value;
+    class pair
+    {
+        int key;
+        int value;
         
-//         pair(int key,int value)
-//         {
-//             this.key=key;
-//             this.value=value;
-//         }        
-//     }
-//     public List<Integer> findClosestElements(int[] arr, int k, int x) {        
+        pair(int key,int value)
+        {
+            this.key=key;
+            this.value=value;
+        }        
+    }
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {        
         
           
-//         int n=arr.length;
+        int n=arr.length;
         
-//         List<Integer>l=new ArrayList<>();
+        List<Integer>l=new ArrayList<>();
         
-//         // max heap
-//         PriorityQueue<pair>pq=new PriorityQueue<>(new Comparator<pair>()
-//                                                   {
-//                                                       public int compare(pair a,pair b)
-//                                                       {
-//                                                           if((b.key-a.key)==0)
-//                                                           {
-//                                                               return b.value-a.value;
-//                                                           }
-//                                                           else
-//                                                           {
-//                                                               return b.key-a.key;
-//                                                           }
-//                                                       }
-//                                                   });
+        // max heap
+        PriorityQueue<pair>pq=new PriorityQueue<>(new Comparator<pair>()
+                                                  {
+                                                      public int compare(pair a,pair b)
+                                                      {
+                                                          if((b.key-a.key)==0)
+                                                          {
+                                                              return b.value-a.value;
+                                                          }
+                                                          else
+                                                          {
+                                                              return b.key-a.key;
+                                                          }
+                                                      }
+                                                  });
         
-//         for(int val:arr)
-//         {
-//             pq.add(new pair(Math.abs(val-x),val));           
+        for(int val:arr)
+        {
+            pq.add(new pair(Math.abs(val-x),val));           
                    
-//              if(pq.size()!=0)
-//                    {
-//                        pq.poll();
-//                    }
-//         }
+             if(pq.size()>k)
+                   {
+                       pq.poll();
+                   }
+        }
                    
-//                    while(pq.size()!=0)
-//                    {
-//                        pair p=pq.poll();
-//                        l.add(p.value);
-//                    }
+                   while(pq.size()!=0)
+                   {
+                       pair p=pq.poll();
+                       l.add(p.value);
+                   }
         
-//                    Collections.sort(l);
-//                    return l;
+                   Collections.sort(l);
+                   return l;
+    }
+}
+
+
+
+// class Solution {
+
+// class Pair{
+//     int key;
+//     int value;
+    
+//     Pair(int key, int value){
+//         this.key = key;
+//         this.value = value;
 //     }
+    
 // }
 
-class Solution {
-
-class Pair{
-    int key;
-    int value;
+// public List<Integer> findClosestElements(int[] arr, int k, int x) {
+//     int n = arr.length;
+//     List<Integer> list = new ArrayList<>();
     
-    Pair(int key, int value){
-        this.key = key;
-        this.value = value;
-    }
+//     // make max priority queue
+//     PriorityQueue<Pair> pq = new PriorityQueue<>(
+//         new Comparator<Pair>(){
+//             public int compare(Pair a, Pair b){
+//                 // if both the keys are same then compare on the bases of their values
+//                 if((b.key - a.key) == 0)
+//                     return b.value - a.value;
+//                 return b.key - a.key;
+//             }
+//     });
     
-}
-
-public List<Integer> findClosestElements(int[] arr, int k, int x) {
-    int n = arr.length;
-    List<Integer> list = new ArrayList<>();
-    
-    // make max priority queue
-    PriorityQueue<Pair> pq = new PriorityQueue<>(
-        new Comparator<Pair>(){
-            public int compare(Pair a, Pair b){
-                // if both the keys are same then compare on the bases of their values
-                if((b.key - a.key) == 0)
-                    return b.value - a.value;
-                return b.key - a.key;
-            }
-    });
-    
-    for(int i: arr){
-        pq.add(new Pair(Math.abs(i-x), i));
+//     for(int i: arr){
+//         pq.add(new Pair(Math.abs(i-x), i));
         
-        if(pq.size() > k)
-            pq.poll();
-    }
+//         if(pq.size() > k)
+//             pq.poll();
+//     }
     
-    while(pq.size() != 0){
-        Pair p = pq.poll();
-        list.add(p.value);
-    }
+//     while(pq.size() != 0){
+//         Pair p = pq.poll();
+//         list.add(p.value);
+//     }
     
-    Collections.sort(list);
-    return list;
-}
-}
+//     Collections.sort(list);
+//     return list;
+// }
+// }
