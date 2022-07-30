c0f395b5b3f3a67d46414c60f6417495f4ed779e
Gurucharan Chouhan <78747692+guruchara@users.noreply.github.com>
2022-07-30 23:04:29 +0530

    Time: 119 ms (20.00%), Space: 43.1 MB (15.00%) - LeetHub

---
+++ 330-patching-array/330-patching-array.js
@@ -0,0 +1,7 @@
+function minPatches(nums, n) {
+	for (var sum = 0, idx = 0, added = 0; sum < n;) {
+		added += idx === nums.length || nums[idx] > sum + 1 ? 1 : 0;
+		sum += (nums[idx] || n) > sum + 1 ? sum + 1 : nums[idx++];
+	}
+	return added;
+}
