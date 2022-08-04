6528244101353b633285554d5e4281ca46a67d94
Gurucharan Chouhan <78747692+guruchara@users.noreply.github.com>
2022-08-05 00:22:40 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (18.06%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.java
@@ -0,0 +1,16 @@
+public class Solution{
+public int mirrorReflection(int p, int q) {
+
+	int height = q;
+	boolean left = false;
+
+	while(height % p != 0) {
+		height += q;
+		left = !left;
+	}
+	if(left)
+		return 2;
+	else 
+		return (height/p)%2;
+}
+}
