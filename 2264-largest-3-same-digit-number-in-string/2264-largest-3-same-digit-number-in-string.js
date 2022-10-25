748f3ebe2a80ac1d2de1e22980dc548ffd7e9187
Gurucharan Chouhan <78747692+guruchara@users.noreply.github.com>
2022-10-25 10:16:06 +0530

    Time: 112 ms (51.63%), Space: 42.5 MB (87.58%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.js
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.js
@@ -16,18 +16,7 @@ var largestGoodInteger = function(num) {
      }
     return ''
 };
- // public String largestGoodInteger(String num) {
- //        String res = "";
- //        int n= num.length();
- //        if(n==0)
- //            return res;
- //    String arr[]= {"999","888","777","666","555","444","333","222","111","000"};
- //        for(int i=0;i<arr.length;i++){
- //            if(num.indexOf(arr[i])>=0){
- //                return arr[i];
- //            }
- //        }
- //        return res;
+
         
 
 
